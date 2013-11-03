1d2382d9aa77ef9ad8a28771580b97e4120e26c9
Spencer Murphy <murphysw@rose-hulman.edu>
2013-11-03 14:25:56 -0500

    Added good start to file watching. It watches files, but doesn't load them yet

---
+++ SimpleWebServer/src/plugin/PluginManager.java
@@ -0,0 +1,199 @@
+/*
+ * PluginManager.java
+ * Nov 1, 2013
+ *
+ * Simple Web Server (SWS) for EE407/507 and CS455/555
+ * 
+ * Copyright (C) 2011 Chandan Raj Rupakheti, Clarkson University
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License 
+ * as published by the Free Software Foundation, either 
+ * version 3 of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ * 
+ * Contact Us:
+ * Chandan Raj Rupakheti (rupakhcr@clarkson.edu)
+ * Department of Electrical and Computer Engineering
+ * Clarkson University
+ * Potsdam
+ * NY 13699-5722
+ * http://clarkson.edu/~rupakhcr
+ */
+
+package plugin;
+
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ * @author Spencer Murphy
+ */
+public class PluginManager implements Runnable{
+	private final WatchService watcher;;
+    private final Map<WatchKey,Path> keys;
+	
+	public PluginManager(String path) throws IOException{
+		Path dir = Paths.get("plugins");
+		this.watcher = FileSystems.getDefault().newWatchService();
+        this.keys = new HashMap<WatchKey,Path>();
+
+        System.out.format("Scanning %s ...\n", dir);
+        registerAll(dir);
+        System.out.println("Done.");
+        
+	}
+	
+    @SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>)event;
+    }
+
+	/**
+	 * 
+	 */
+	public void watch() {
+		for (;;) {
+
+            // wait for key to be signalled
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch (InterruptedException x) {
+                return;
+            }
+
+            Path dir = keys.get(key);
+            if (dir == null) {
+                System.err.println("WatchKey not recognized!!");
+                continue;
+            }
+
+            for (WatchEvent<?> event: key.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+
+                // Context for directory entry event is the file name of entry
+                WatchEvent<Path> ev = cast(event);
+                Path name = ev.context();
+                Path child = dir.resolve(name);
+
+                // print out event
+                System.out.format("%s: %s\n", event.kind().name(), child);
+
+                // if directory is created, then
+                // register it and its sub-directories
+                if (kind == ENTRY_CREATE) {
+                    try {
+                        if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
+                            registerAll(child);
+                        }
+                        System.out.println("added");
+                    } catch (IOException x) {
+                        // ignore to keep sample readbale
+                    }
+                }
+                else if (kind == ENTRY_MODIFY){
+                	try{
+                		if (Files.isDirectory(child,  NOFOLLOW_LINKS)) {
+                			registerAll(child);
+                		}
+                		System.out.println("modified");
+                	}
+                	catch (Exception e) {
+						// TODO: handle exception
+					}
+                }
+                else if (kind == ENTRY_DELETE){
+                	try{
+                		if (Files.isDirectory(child,  NOFOLLOW_LINKS)) {
+                			//remove keys
+                			
+                		}
+                		System.out.println("deleted");
+                	}
+                	catch (Exception e) {
+						// TODO: handle exception
+					}
+                }
+            }
+
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+            }
+        }
+		
+		
+		
+	}
+	
+    private void register(Path dir) throws IOException {
+        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        Path prev = keys.get(key);
+        if (prev == null) {
+            System.out.format("register: %s\n", dir);
+        } else {
+            if (!dir.equals(prev)) {
+                System.out.format("update: %s -> %s\n", prev, dir);
+            }
+        }
+        keys.put(key, dir);
+    }
+
+    /**
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
+     */
+    private void registerAll(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                throws IOException
+            {
+                register(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		watch();
+		
+	}
+
+}

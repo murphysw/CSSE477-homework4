32dff213f3d352e0a2d9afe366558cd691bbca35
hoorncj <hoorncj@rose-hulman.edu>
2013-11-03 15:14:29 -0500

    Added Plugin jar

--- ServerPlugin1/src/mine/Plugin1Main.java
+++ ServerPlugin1/src/mine/Plugin1Main.java
@@ -8,6 +8,11 @@ import plugin.AbstractPlugin;
 public class Plugin1Main extends AbstractPlugin {
 	
 	private final String pathToConfigFile = "Plugin1ServletConfigFile.txt";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
 
 	@Override
 	public File getConfigFile() {
